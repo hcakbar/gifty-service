c85f41ac1a667d77f4fbe87228bdf9c93f9b8507
hcakbar <hc.akbar@gmail.com>
2017-08-20 16:11:07 -0400

    adding poc integration test

--- src/test/java/com/gifty/service/DemoResourcesTest.java
+++ src/test/java/com/gifty/service/DemoResourcesTest.java
@@ -1,6 +1,12 @@
 package com.gifty.service;
 
+import org.json.JSONException;
 import org.junit.Test;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 
 import static org.junit.Assert.assertEquals;
 
@@ -19,6 +25,17 @@ public class DemoResourcesTest {
     @Test
     public void testAddTwoNumbers() {
         assertEquals(6, demoResources.addTwoNumbers(3, 3));
+    }
+
+    //Integration test with SpringBoot TestRestTemplate
+    @Test
+    public void verifyHiResourceReturnsValidResponse() throws JSONException {
+        HttpHeaders headers = new HttpHeaders();
+        TestRestTemplate restTemplate = new TestRestTemplate();
+        HttpEntity<String> entity = new HttpEntity<> (null, headers);
+        ResponseEntity<String> response = restTemplate.exchange("http://localhost:8080/hi", HttpMethod.GET, entity, String.class);
+        assertEquals(200, response.getStatusCodeValue());
+        assertEquals("hello", response.getBody());
     }
 
 }
